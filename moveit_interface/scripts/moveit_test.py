b1c6947fafc777e5cf5e3049d3b7f6e2d27c0f82
gabrielcalmon <jgabrielcalmon1@hotmail.com>
2023-04-18 13:51:12 -0300

    organize exemple execution

--- moveit_interface/scripts/moveit_test.py
+++ moveit_interface/scripts/moveit_test.py
@@ -245,6 +245,7 @@ def demo_function():
     print('Going to home position')
     go2init_pose()
 
+    # zy
     input('===Press enter to use "position control"===\n')
     go2position(pointA)
     go2position(pointB)
@@ -327,35 +328,27 @@ if __name__ == '__main__':
     # print('Finished!!')
     # print_pose()
 
-## pose test script (yz)
-    # zstep=0.2
-    # ystep=0.5
+## pose test script (yz) da demo_function
+    zstep=0.2
+    ystep=0.5
 
-    # #pointA=[0.5147866096539608, 0.07352425677103026, 0.2379017918991315]
-    # pointA = [0.46567264870156727, 0.32583083817351666, 0.22664581792749117]
-    # pointB=[pointA[0], pointA[1], pointA[2]+zstep]
-    # pointC=[pointB[0], pointB[1]-ystep, pointB[2]]
-    # pointD=[pointC[0], pointC[1], pointC[2]-zstep]
-    # print('Going to home position')
-    # go2init_pose()
-    # #input('===Get at home position===')
+    pointA = [0.46567264870156727, 0.32583083817351666, 0.22664581792749117]
+    pointB=[pointA[0], pointA[1], pointA[2]+zstep]
+    pointC=[pointB[0], pointB[1]-ystep, pointB[2]]
+    pointD=[pointC[0], pointC[1], pointC[2]-zstep]
+    print('Going to home position')
+    go2init_pose()
 
-    # go2pose(pointA)
-    # #input('===Press enter to go to the next point===\n')
+    # zy
+    input('===Press enter to use "pose control"===\n')
+    go2init_pose()
+    go2pose(pointA)
+    go2pose(pointB)
+    go2pose(pointC)
+    go2pose(pointD)
+    go2pose(pointA)
+    print('Finished!!')
 
-    # go2pose(pointB)
-    # #input('===Press enter to go to the next point===\n')
-
-    # go2pose(pointC)
-    # #input('===Press enter to go to the next point===\n')
-
-    # go2pose(pointD)
-    # #input('===Press enter to go to the next point===\n')
-
-    # go2pose(pointA)
-    # print('Finished!!')
-    # print_pose()
-
-    cartesianMove()
+    # cartesianMove()
     #init_ang()
     #demo_function()
