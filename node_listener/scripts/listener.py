b6a4eb91732d56ec727bce2c06effc1d21d132a2
gabrielcalmon <jgabrielcalmon1@hotmail.com>
2023-04-18 15:14:29 -0300

    fix csv import format problem

--- node_listener/scripts/listener.py
+++ node_listener/scripts/listener.py
@@ -5,28 +5,41 @@ import csv
 import datetime
 
 sub_amostragem = 100 # so sera registrada no arquivo uma a cada N mensagens
-decimal_precision = 50
+# por padrao /joint_states opera a 998hz
+#float_precision = 50
 
-now = datetime.datetime.now()
+now = datetime.datetime.now() # recebe o horario atual
 filename = 'src/time_series_csv/joint_states_{}.csv'.format(now.strftime('%Y%m%d_%H%M%S'))  # cria um nome unico para cada nova execucao no diretorio especificado
 
 count = 0
+
+#substitui o marcador decimal ponto por virgula
+def localize_floats(row):
+    return [
+        str(el).replace('.', ',') for el in row
+    ]
+
 def joint_state_callback(data):
     global count
     count += 1
     
+    # realiza a subamostragem
     if count % sub_amostragem == 0:
         rospy.loginfo(rospy.get_caller_id() + "I heard %s", data)
         # Abre o arquivo CSV e adiciona uma nova linha com os dados atuais
         with open(filename, mode='a') as csv_file:
-            writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            writer = csv.writer(csv_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
             # cria o cabecalho do csv CASO o arquivo esteja vazio
             if csv_file.tell() == 0:
                 writer.writerow(['time', 'pos_1', 'pos_2', 'pos_3', 'pos_4', 'pos_5', 'pos_6', 'vel_1', 'vel_2', 'vel_3', 'vel_4', 'vel_5', 'vel_6', 'eff_1', 'eff_2', 'eff_3', 'eff_4', 'eff_5', 'eff_6'])
-            write_list=list(data.position) + list(data.velocity) + list(data.effort)
-            formattedList = [data.header.stamp.to_sec()] + [f'%.{decimal_precision}f' % x for x in write_list]
-            writer.writerow(formattedList)
+            #write_list=list(data.position) + list(data.velocity) + list(data.effort)
+            #formattedList = [data.header.stamp.to_sec()] + [f'%.{float_precision}f' % x for x in write_list]
+            
+            # concatena os dados da mensagem numa lista
+            formattedList = [data.header.stamp.to_sec()] + list(data.position) + list(data.velocity) + list(data.effort)
+            #formattedList = map(float, formattedList)
+            writer.writerow(localize_floats(formattedList))
 
 def joint_state_subscriber():
     rospy.init_node('joint_state_listener', anonymous=True)
