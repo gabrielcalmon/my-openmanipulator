60e504e15398fc0269ab98b1b4b7c6f6a8bfae0c
gabrielcalmon <jgabrielcalmon1@hotmail.com>
2023-05-12 15:24:40 -0300

    fix callback error

--- moveit_interface/scripts/move_to.py
+++ moveit_interface/scripts/move_to.py
@@ -38,12 +38,10 @@ def init_pose():
     rospy.sleep(1)
 
 def go2position(position_list):
-    moveit_commander.roscpp_initialize(sys.argv)
-    rospy.init_node("movit_joint_test", anonymous=True)
     robot = moveit_commander.RobotCommander()
     group_name = "arm"
     group = moveit_commander.MoveGroupCommander(group_name)
-
+    #group.set_goal_tolerance(0.01)
     while not rospy.is_shutdown():
         group.set_position_target(position_list)
         plan = group.go(wait=True)
@@ -54,11 +52,10 @@ def go2position(position_list):
     #print()
 
 def go2pose(position_list):
-    moveit_commander.roscpp_initialize(sys.argv)
-    rospy.init_node("movit_joint_test", anonymous=True)
     robot = moveit_commander.RobotCommander()
     group_name = "arm"
     group = moveit_commander.MoveGroupCommander(group_name)
+    #group.set_goal_tolerance(0.1)
 
     while not rospy.is_shutdown():
         pose_goal = geometry_msgs.msg.Pose()
@@ -80,18 +77,29 @@ def go2pose(position_list):
         break
 
 def callback(incoming_pose):
+    safe_dist = 0.0
     pose_msg = geometry_msgs.msg.Pose()
     pose_msg.position.x = incoming_pose.position.x
     pose_msg.position.y = incoming_pose.position.y
     pose_msg.position.z = incoming_pose.position.z
-    go2position([pose_msg.position.x, pose_msg.position.y, pose_msg.position.z])
-    
+    pose_msg.orientation.x = incoming_pose.orientation.x
+    pose_msg.orientation.y = incoming_pose.orientation.y
+    pose_msg.orientation.z = incoming_pose.orientation.z
+    pose_msg.orientation.w = incoming_pose.orientation.w
+    # esses valores somados sao um ajuste tecnico para contornar a diferenca entre a tf e a posicao real do aruco
+    go2position([pose_msg.position.x-safe_dist, pose_msg.position.y+0.25-safe_dist, pose_msg.position.z+0.20-safe_dist])
+    #go2pose([pose_msg.position.x-safe_dist, pose_msg.position.y-safe_dist, pose_msg.position.z-safe_dist])
+    # print(pose_msg.position)
+
+    #go2pose([0.46567264870156727, 0.32583083817351666, 0.22664581792749117])
+    #go2position([0.46567264870156727, 0.32583083817351666, 0.22664581792749117])
+    #print(incoming_pose)
     #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
 
 def listener():
-    rospy.init_node('target_calculator/pose_world2tag', anonymous=True)
+    rospy.init_node('target_listener')
 
-    rospy.Subscriber("target_listener", geometry_msgs.msg.Pose, callback)
+    rospy.Subscriber("target_calculator/pose_world2tag", geometry_msgs.msg.Pose, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
@@ -99,5 +107,8 @@ def listener():
 if __name__ == '__main__':
     try:
         listener()
+        moveit_commander.roscpp_initialize(sys.argv)
+        rospy.init_node("moveit_joint_test", anonymous=True)
+        
     except rospy.ROSInterruptException:
         pass
